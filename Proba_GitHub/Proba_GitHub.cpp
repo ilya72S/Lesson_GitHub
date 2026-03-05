7276f9829d354027403cfc58aa58a938d0b68795
Илья Шишкин <i7272@mail.ru>
2026-03-05 14:02:42 +0300

    Reapply "3"
    This reverts commit 800011cbc308d6b171bd82b62fea22ec59d1e145.

--- Proba_GitHub/Proba_GitHub.cpp
+++ Proba_GitHub/Proba_GitHub.cpp
@@ -2,9 +2,14 @@
 //
 
 #include <iostream>
+#include <locale.h>
+#include <cstdlib>
+using namespace std;
 
 int main()
 {
+    setlocale(LC_ALL, "Russian");
+
     std::cout << "Hello World!\n";
     return 0;
 }
